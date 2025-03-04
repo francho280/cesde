70f8c9fbd3ea92c1995601e0acc8f39e6b2d4879
francho280 <franbuho@gmail.com>
2025-03-04 16:13:08 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BlogDeCafe</title>
+    <meta name="description" content="Página web de blog de café">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- Prefetch-->
+    <link rel="Prefetch" href="nosotros.html" as="document">
+    <!--preload-->
+
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&family=Open+Sans&family=PT+Sans:wght@400;700&family=Staatliches&display=swap" crossorigin="crossorigin" as="font">
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&family=Open+Sans&family=PT+Sans:wght@400;700&family=Staatliches&display=swap" rel="stylesheet">
+
+    <link rel="preload" href="CSS/normalize.css" as="style">
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <link rel="preload" href="CSS/style.css" as="style">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <header class="header">
+
+        <div class="contenedor">
+            <div class="barra">
+                <a class="logo" href="index.php">
+                    <h1 class="logo__nombre no-margin centrar-texto">CESDE</h1>
+                </a>
+
+            </div>
+        </div>
+
+        <div class="header__texto">
+           
+        </div>
+    </header>
+    <main>
+        <nav>
+            <a href="admin/index.php">Admin</a>
+        </nav>
+    </main>
+    
+    
+    <footer class="footer">
+        <div class="contenedor">
+            <div class="barra">
+                <a class="logo" href="index.html">
+                    <h1 class="logo__nombre no-margin centrar-texto">CESDE</span></h1>
+                </a>
+            
+               
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/modernizr.js"></script>
+</body>
+</html>
+
